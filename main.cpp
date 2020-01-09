8ea12f5182f446a2ce17f71419854bf93288800d
Yan.Hu <yzhu798@gmail.com>
2020-01-10 02:00:19 +0800

    just test first

---
+++ main.cpp
@@ -0,0 +1,14 @@
+﻿#include "mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    qputenv("QTWEBENGINE_REMOTE_DEBUGGING", "7777");
+    QCoreApplication::setOrganizationName("QtExamples");
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QApplication app(argc, argv);
+    MainWindow w;
+    w.show();
+    return app.exec();
+}
